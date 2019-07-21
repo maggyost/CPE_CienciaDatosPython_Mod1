7d7ff373a79e313bd6c22f9d6e309d2c4c07f793
Private <jgomezz@gmail.com>
2019-07-21 13:19:51 -0500

    Sesion 01

---
+++ variables.py
@@ -0,0 +1,35 @@
+'''
+Programa : Ciencia de Datos con Pthon
+Modulo 01 : Fundamentos de Python para Ciencia de Datos
+Sesion 01 : Introduccion
+Tema : Variables
+Fecha : 21/07/2019
+Version : 1
+Author :
+'''
+
+
+x = 1 + 2 + 3.0 + int("4")
+
+print(x)
+print(type(x))
+
+x = x + 1 + 2
+
+print(x)
+print(type(x))
+
+x = 3/2
+
+print(x)
+print(type(x))
+
+x = 3//2
+
+print(x)
+print(type(x))
+
+x = int(3/2)
+
+print(x)
+print(type(x))
