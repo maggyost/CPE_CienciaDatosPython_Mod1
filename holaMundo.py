c1f562e443029194eabdbf15c119db8e0d9226d6
Private <jgomezz@gmail.com>
2019-07-21 12:19:56 -0500

    Sesion 01

---
+++ holaMundo.py
@@ -0,0 +1,52 @@
+'''
+Programa : Ciencia de Datos con Pthon
+Modulo 01 : Fundamentos de Python para Ciencia de Datos
+Sesion 01 : Introduccion
+Fecha : 21/07/2019
+Version : 1
+Author :
+'''
+
+
+# Comentario en Python
+print("Hola Mundo ..!")
+
+''' Otra forma de comentario en Python '''
+print("Hola Mundo 2 ..!")
+
+# Comentario de linea 1
+# Comentario de linea 2
+print("Hola Mundo 3 ..!") # Imprime
+
+'''
+Comentario de linea 1
+Comentario de linea 2 '''
+print("Hola Mundo 4 ..!") #''' Imprime '''
+
+'''
+print("Hola Mundo 5 ..!")
+print("Hola Mundo 6 ..!")
+frint("Hola Mundo 7 ..!")
+print("Hola Mundo 8 ..!")
+print("Hola Mundo 9 ..!")
+print("Hola Mundo 10 ..!")
+#'''
+
+print("Hola Mundo 11 ..!")
+
+
+def imprimir(msg):
+    '''
+    Esta funcion imprime un mensaje en la pantalla
+    :param msg: Valor a imprimir
+    :return: Mostrar msg por pantalla
+    '''
+    print(msg)
+    print(msg)
+    print(msg)
+    print(msg)
+    print(msg)
+
+imprimir("Hola Mundo desde funcion")
+imprimir("Numero")
+
