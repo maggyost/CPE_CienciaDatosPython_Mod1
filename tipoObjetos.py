f14d74f2672868d87bfdde288ae980257e43cbe1
Private <jgomezz@gmail.com>
2019-07-21 12:31:44 -0500

    Sesion 01

---
+++ tipoObjetos.py
@@ -0,0 +1,9 @@
+'''
+Programa : Ciencia de Datos con Pthon
+Modulo 01 : Fundamentos de Python para Ciencia de Datos
+Sesion 01 : Introduccion
+Tema : Tipo de objetos
+Fecha : 21/07/2019
+Version : 1
+Author :
+'''
